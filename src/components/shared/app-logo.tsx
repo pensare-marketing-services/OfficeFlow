d63f94bff241d6a3e5f094bd56fd1a6a3a5bbbb2
Pensare Marketing <pensaremail@gmail.com>
2025-12-05 09:15:01 +0000

    the structure i want this image as main logo in app

--- src/components/shared/app-logo.tsx
+++ src/components/shared/app-logo.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export function AppLogo() {
     return (
         <div className="flex h-10 w-10 items-center justify-center rounded-lg bg-primary">
-            <Image src="/app-logo.png" alt="OfficeFlow Logo" width={24} height={24} />
+            <Image src="/avatars/app-logo.png" alt="OfficeFlow Logo" width={24} height={24} />
         </div>
     );
 }
