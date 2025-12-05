c109093b03b48719c6b27e3556b0b3d2061656a5
Pensare Marketing <pensaremail@gmail.com>
2025-12-05 10:28:05 +0000

    Noto Sans Thaana SANSSERIF

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,7 +10,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['"Noto Sans Thaana"', 'serif'],
+        body: ['"Noto Sans Thaana"', 'sans-serif'],
         headline: ['"Poppins"', 'sans-serif'],
         code: ['monospace'],
       },
