6763ca86b6d23bfda4323c129b34c4eca662fb36
Pensare Marketing <pensaremail@gmail.com>
2025-12-05 10:22:18 +0000

    instad of ptsans use ubuntu font'

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,7 +10,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['"PT Sans"', 'sans-serif'],
+        body: ['"Ubuntu"', 'sans-serif'],
         headline: ['"Poppins"', 'sans-serif'],
         code: ['monospace'],
       },
