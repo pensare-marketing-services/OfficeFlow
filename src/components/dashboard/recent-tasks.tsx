a8d0cedbf86416fb0fa5181b9b107c61d594ec74
Pensare Marketing <pensaremail@gmail.com>
2025-12-21 06:50:51 +0000

    dont want .w-12 {     width: 3rem; }
    just make column width 1 rem of o

--- src/components/dashboard/recent-tasks.tsx
+++ src/components/dashboard/recent-tasks.tsx
@@ -116,7 +116,7 @@ const EditablePriorityCell = ({ task, onUpdate }: { task: Task & {id: string}, o
                 onChange={(e) => setPriority(Number(e.target.value))}
                 onBlur={handleSave}
                 onKeyDown={handleKeyDown}
-                className="h-7 w-12 text-xs p-1 mx-auto"
+                className="h-7 w-10 text-xs p-1 mx-auto"
             />
         </TableCell>
     );
@@ -267,7 +267,7 @@ export default function RecentTasks({ tasks, users, title, onTaskDelete }: Recen
           <TableHeader>
             <TableRow>
               <TableHead className="py-1 px-2 border-r border-t w-[25px] text-[10px] h-8">#</TableHead>
-              <TableHead className="py-1 px-2 border-r border-t text-[10px] h-8 w-[60px]">Order</TableHead>
+              <TableHead className="py-1 px-2 border-r border-t text-[10px] h-8 w-14">Order</TableHead>
               <TableHead className="py-1 px-2 border-r border-t text-[10px] h-8" style={{width: '100px'}}>Client</TableHead>
               <TableHead className="py-1 px-2 border-r border-t text-[10px] h-8" style={{width: '200px'}}>Task</TableHead>
                {isAdmin && <TableHead className="py-1 px-2 border-r border-t text-[10px] h-8">Assigned</TableHead>}
