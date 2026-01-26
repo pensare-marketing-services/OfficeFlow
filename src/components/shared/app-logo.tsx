ee05fa1439521ad7fdebef7817a406d2ba738119
Pensare Marketing <pensaremail@gmail.com>
2026-01-26 06:01:01 +0000

    chnaghe the client logo in sidepanel

--- src/components/shared/app-logo.tsx
+++ src/components/shared/app-logo.tsx
@@ -2,8 +2,8 @@ import Image from 'next/image';
 
 export function AppLogo() {
     return (
-        <div className="flex h-10 w-60 m-6 justify-start rounded-lg ">
-            <Image src="/avatars/app-logo.png" alt="OfficeFlow Logo" width={200} height={90} />
+        <div className="flex h-12 items-center justify-start px-4">
+            <Image src="/avatars/app-logo.png" alt="OfficeFlow Logo" width={150} height={40} style={{ objectFit: 'contain' }} />
         </div>
     );
 }
